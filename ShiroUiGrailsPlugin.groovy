dc36674e39edb137608a79d4e912f4ebabf77034
yellowsnow <melaoufir@gmail.com>
2011-08-12 11:15:33 +0000

    Fix for issue #1

--- ShiroUiGrailsPlugin.groovy
+++ ShiroUiGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class ShiroUiGrailsPlugin {
     // the plugin version
     def version = "1.0-beta2"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.7 > *"
+    def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
     def dependsOn = [shiro:'1.1.3 > *', mail: '1.0-SNAPSHOT > *']
     // resources that are excluded from plugin packaging
